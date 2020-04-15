21a70534558c6fab08ce9f99c0131e985960806e
Michiel Peters <wishjuh@gmail.com>
2020-04-15 22:57:40 +0200

    css modules + layout

---
+++ src/components/layout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import style from './layout.module.css';
+
+export default ({ children }) => <main className={style.main}>{children}</main>;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 export default () => (
-	<main>
-		<h1>Michiel.is</h1>
-	</main>
+	<Layout>
+		<main>
+			<h1>Michiel.is</h1>
+		</main>
+	</Layout>
 );
