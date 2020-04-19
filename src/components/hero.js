1577a655bd571fb1f053b80787b0fdd60db8d639
Michiel Peters <wishjuh@gmail.com>
2020-04-19 16:34:36 +0200

    I am

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,6 +5,9 @@ import style from './hero.module.css';
 export default () => (
 	<section className={style.hero}>
 		<h1>Michiel.is</h1>
-		<p>Having fun &#x1F643;</p>
+		<p>
+			Developer, maker, IoT enthusiast, &#x1F970;connected experiences & phygital, vlerk
+			&#x1F984;&#x1F996;&#x1F412;
+		</p>
 	</section>
 );
