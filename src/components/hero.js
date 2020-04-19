b9076a2de9419be3c880bf9a5c700f1d56dcfe9b
Michiel Peters <wishjuh@gmail.com>
2020-04-19 21:30:58 +0200

    look mom, animations

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,8 +4,8 @@ import style from './hero.module.css';
 
 export default () => (
 	<section className={style.hero}>
-		<h1>Michiel.is</h1>
-		<p>
+		<h1 className={`${style.hero__heading} ${style.animate__popIn}`}>Michiel.is</h1>
+		<p className={`${style.hero__sub} ${style.animate__popIn}`}>
 			Developer, maker, IoT enthusiast, &#x1F970;connected experiences & phygital, vlerk
 			&#x1F984;&#x1F996;&#x1F412;
 		</p>
