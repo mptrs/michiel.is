78dd8460d08235a9759bf715f7a152d6d5e645ec
Michiel Peters <wishjuh@gmail.com>
2020-04-19 17:08:22 +0200

    small screen tweak

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,13 @@
  */
 
 module.exports = {
-	/* Your site config here */
-
+	siteMetadata: {
+		title:
+			'Michiel.is Developer, maker, IoT enthusiast, &#x1F970; connected experiences & phygital, vlerk &#x1F984;&#x1F996;&#x1F412;',
+		description: '',
+		url: 'https://michiel.is',
+		image: '',
+		twitterUsername: '@wishjuh',
+	},
 	plugins: [`gatsby-plugin-react-helmet`],
 };
